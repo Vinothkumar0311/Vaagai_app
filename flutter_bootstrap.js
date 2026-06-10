b412131c46c506d6a36bdd4c9f47f6e3f1c235c7
Vinothkumar0311 <91531509+Vinothkumar0311@users.noreply.github.com>
2026-06-10 15:23:29 +0000

    Deploying to gh-pages from @ Vinothkumar0311/Vaagai_app@2b1fa4b4a30f62253254c44f1336fe9d833ce270 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3467415723" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4152803879" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
